3612cf4cf53deb368da1f9373c6417abe5506da3
David Lam <lamd@rose-hulman.edu>
2016-04-20 20:03:02 +0000

    done interrupt handler

--- m2/kernel.asm
+++ m2/kernel.asm
@@ -6,7 +6,7 @@
 	.global _putInMemory
 	.global _interrupt
 	.global _makeInterrupt21
-;	.extern _handleInterrupt21
+	.extern _handleInterrupt21
 
 ;void putInMemory (int segment, int address, char character)
 _putInMemory:
@@ -46,7 +46,7 @@ intr:	int #0x00	;call the interrupt (00 will be changed above)
 
 ;void makeInterrupt21()
 ;this sets up the interrupt 0x21 vector
-;when an interrupt 0x21 is called in the future, 
+;when an interrupt 0x21 is called in the future,
 ;_interrupt21ServiceRoutine will run
 
 _makeInterrupt21:
@@ -66,14 +66,14 @@ _makeInterrupt21:
 ;it will call your function:
 ;void handleInterrupt21 (int AX, int BX, int CX, int DX)
 _interrupt21ServiceRoutine:
-;	push dx
-;	push cx
-;	push bx
-;	push ax
-;	call _handleInterrupt21
-;	pop ax
-;	pop bx
-;	pop cx
-;	pop dx
+	push dx
+	push cx
+	push bx
+	push ax
+	call _handleInterrupt21
+	pop ax
+	pop bx
+	pop cx
+	pop dx
 
-;	iret
+	iret
